1f8e68be4b9cf86896000a50a6747d49772d3b27
AlexHedre <alex.hedre@gmail.com>
2021-02-22 17:59:29 +0100

    Added new folder database

---
+++ src/database/Record.java
@@ -0,0 +1,31 @@
+package database;
+
+import staff.*;
+
+public class Record {
+    private Patient patient;
+    private Doctor doctor;
+    private Nurse nurse;
+    private Division division;
+    private String data;
+
+    public Record(Patient patient, Doctor doctor, Nurse nurse, String data) {
+        this.patient = patient;
+        this.doctor = doctor;
+        this.nurse = nurse;
+        this.division = doctor.getDivision();
+        this.data = data;
+    }
+
+    public boolean canRead(Person person) {
+        return (person.getId() == patient.getId());
+    }
+
+    public boolean canWrite(Person person) {
+        return (person.getId() == doctor.getId() || person.getId() == nurse.getId());
+    }
+
+    public void writeToRecord(String data) {
+        this.data = data;
+    }
+}
