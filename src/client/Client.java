1b07be784a7471d34e1617706262eee424ab8cbe
AlexHedre <alex.hedre@gmail.com>
2021-02-22 17:44:38 +0100

    Implemented code from project 1.

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,4 +1,99 @@
 package client;
 
+import javax.net.ssl.*;
+import javax.security.cert.X509Certificate;
+import java.io.*;
+import java.security.KeyStore;
+
 public class Client {
+
+    public static void main(String[] args) throws Exception {
+        String host = null;
+        int port = -1;
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args[" + i + "] = " + args[i]);
+        }
+        if (args.length < 2) {
+            System.out.println("USAGE: java client host port");
+            System.exit(-1);
+        }
+        try { /* get input parameters */
+            host = args[0];
+            port = Integer.parseInt(args[1]);
+        } catch (IllegalArgumentException e) {
+            System.out.println("USAGE: java client host port");
+            System.exit(-1);
+        }
+
+        /*** GET USERNAME AND PASSWORD ***/
+        String[] msg = new String[2];
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter Username:");
+        msg[0] = read.readLine();
+        System.out.println("Enter Password:");
+        msg[1] = read.readLine();
+
+        try { /* set up a key manager for client authentication */
+            SSLSocketFactory factory = null;
+            try {
+                char[] password = msg[1].toCharArray();
+                KeyStore ks = KeyStore.getInstance("JKS");
+                KeyStore ts = KeyStore.getInstance("JKS");
+                KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+                TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+
+                SSLContext ctx = SSLContext.getInstance("TLS");
+                ks.load(new FileInputStream("Certificates/"), password);  //keystore
+
+                ts.load(new FileInputStream("Certificates/"), "password".toCharArray()); //truststore
+
+                kmf.init(ks, password); // user password (keypass)
+                tmf.init(ts); // keystore can be used as truststore here
+                ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+                factory = ctx.getSocketFactory();
+            } catch (Exception e) {
+                throw new IOException(e.getMessage());
+            }
+
+            SSLSocket socket = (SSLSocket)factory.createSocket(host, port);
+            System.out.println("\nsocket before handshake:\n" + socket + "\n");
+
+            /*
+             * send http request
+             *
+             * See SSLSocketClient.java for more information about why
+             * there is a forced handshake here when using PrintWriters.
+             */
+            socket.startHandshake();
+
+            SSLSession session = socket.getSession();
+            X509Certificate cert = (X509Certificate)session.getPeerCertificateChain()[0];
+            String subject = cert.getSubjectDN().getName();
+            System.out.println("certificate name (subject DN field) on certificate received from server:\n" + subject + "\n");
+            System.out.println("socket after handshake:\n" + socket + "\n");
+            System.out.println("secure connection established\n\n");
+
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while (socket.isConnected()) {
+                String input;
+                while (!(input = in.readLine()).equals("ENDOFMSG")) {
+                    System.out.println(input);
+                }
+                System.out.print(">");
+                msg[0] = read.readLine();
+                if (msg[0].equalsIgnoreCase("quit")) {
+                    break;
+                }
+                out.println(msg[0]);
+                out.flush();
+            }
+            in.close();
+            out.close();
+            read.close();
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,4 +1,101 @@
 package server;
+import java.io.*;
+import java.net.*;
+import java.security.KeyStore;
+import javax.net.*;
+import javax.net.ssl.*;
+import javax.security.cert.X509Certificate;
 
-public class Server {
+public class Server implements Runnable {
+    private ServerSocket serverSocket = null;
+    private static int numConnectedClients = 0;
+
+    public Server(ServerSocket ss) throws IOException {
+        serverSocket = ss;
+        newListener();
+    }
+
+    public void run() {
+        try {
+            SSLSocket socket=(SSLSocket)serverSocket.accept();
+            newListener();
+            SSLSession session = socket.getSession();
+            X509Certificate cert = (X509Certificate)session.getPeerCertificateChain()[0];
+
+            numConnectedClients++;
+
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String subject = cert.getSubjectDN().getName();
+
+            System.out.println("client connected");
+            System.out.println("client name (cert subject DN field): " + subject);
+            System.out.println(numConnectedClients + " concurrent connection(s)\n");
+
+            String clientMsg;
+            while ((clientMsg = in.readLine()) != "" && clientMsg != null && !clientMsg.equals("quit")) {
+                /*** where stuff needs to be done ***/
+
+                out.println("ENDOFMSG".toCharArray());
+            }
+            in.close();
+            out.close();
+            socket.close();
+            numConnectedClients--;
+            System.out.println("client disconnected");
+            System.out.println(numConnectedClients + " concurrent connection(s)\n");
+        } catch (IOException e) {
+            System.out.println("Client died: " + e.getMessage());
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    private void newListener() { (new Thread(this)).start(); } // calls run()
+
+    public static void main(String args[]) {
+        System.out.println("\nServer Started\n");
+        int port = -1;
+        if (args.length >= 1) {
+            port = Integer.parseInt(args[0]);
+        }
+        String type = "TLS";
+        try {
+            ServerSocketFactory ssf = getServerSocketFactory(type);
+            ServerSocket ss = ssf.createServerSocket(port);
+            ((SSLServerSocket)ss).setNeedClientAuth(true); // enables client authentication
+            new Server(ss);
+        } catch (IOException e) {
+            System.out.println("Unable to start Server: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static ServerSocketFactory getServerSocketFactory(String type) {
+        if (type.equals("TLS")) {
+            SSLServerSocketFactory ssf = null;
+            try { // set up key manager to perform server authentication
+                SSLContext ctx = SSLContext.getInstance("TLS");
+                KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+                TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+                KeyStore ks = KeyStore.getInstance("JKS");
+                KeyStore ts = KeyStore.getInstance("JKS");
+                char[] password = "password".toCharArray();
+
+                ks.load(new FileInputStream("Certificates/"), password);  // keystore password (storepass)
+                ts.load(new FileInputStream("Certificates/"), password); // truststore password (storepass)
+                kmf.init(ks, password); // certificate password (keypass)
+                tmf.init(ts);  // possible to use keystore as truststore here
+                ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+                ssf = ctx.getServerSocketFactory();
+                return ssf;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            return ServerSocketFactory.getDefault();
+        }
+        return null;
+    }
 }
