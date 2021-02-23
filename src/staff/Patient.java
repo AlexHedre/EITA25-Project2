c79bb6b195c3ab356fe3315a9cef34fed051c3a8
AlexHedre <alex.hedre@gmail.com>
2021-02-23 11:12:22 +0100

    Added doctor to patient

--- src/staff/Patient.java
+++ src/staff/Patient.java
@@ -1,8 +1,10 @@
 package staff;
 
 public class Patient extends Person {
+    private Doctor doctor;
 
-    public Patient(String name, Division div, String id) {
+    public Patient(String name, Division div, String id, Doctor doctor) {
         super(name, id, div);
+        this.doctor = doctor;
     }
 }
