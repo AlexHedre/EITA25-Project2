dd52ea95648e88cda2976d29670bf38ca9a178d4
AlexHedre <alex.hedre@gmail.com>
2021-02-23 23:14:51 +0100

    Implemented base for PersonInformationManager

--- src/serverUtil/PersonInformationManager.java
+++ src/serverUtil/PersonInformationManager.java
@@ -1,4 +1,34 @@
 package serverUtil;
 
+import staff.*;
+import java.util.*;
+import java.io.*;
+
 public class PersonInformationManager {
+    private final String filePath = "Database/personInformation";
+    private HashMap<String, Person> persons;
+    private ArrayList<Division> divisions;
+
+    public PersonInformationManager () {
+        persons = new HashMap<String, Person>();
+        divisions = new ArrayList<Division>();
+        readFile();
+    }
+
+    public Person getPersonFromId(String id) {
+        if (persons.containsKey(id)) {
+            return persons.get(id);
+        }
+        return null;
+    }
+
+    public void readFile() {
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(filePath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
